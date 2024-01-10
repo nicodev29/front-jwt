79d6024260f86ab94dcada7e159f7118eb8ec196
Nicolas <nicogarcia1229@gmail.com>
2024-01-09 21:44:56 -0300

    update-design

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,7 +1,7 @@
 <template>
-    <div>
-        <h1>Login</h1>
-        <form @submit.prevent="login">
+    <div class="login-container">
+        <h1>JWT LOGIN</h1>
+        <form @submit.prevent="login" class="login-form">
             <input v-model="username" type="text" placeholder="Username" />
             <input v-model="password" type="password" placeholder="Password" />
             <button type="submit">Login</button>
@@ -38,16 +38,49 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 .login-container {
-    max-width: 300px;
-    margin: auto;
+    max-width: 400px;
+    margin: 100px auto;
+    padding: 20px;
+    background: rgba(255, 255, 255, 0.2);
+    /* Transparencia */
+    border-radius: 10px;
+    box-shadow: 0 0 15px rgba(0, 0, 0, 0.2);
     text-align: center;
 }
 
-input {
+.login-form input {
     margin: 10px 0;
-    width: 100%;
+    width: 90%;
+    padding: 10px;
+    border: none;
+    border-radius: 5px;
+    box-shadow: inset 0 0 5px rgba(0, 0, 0, 0.2);
+}
+
+.login-form button {
+    width: 50%;
+    padding: 10px;
+    margin-top: 20px;
+    border: none;
+    border-radius: 5px;
+    background-color: #4CAF50;
+    /* Color del botón */
+    color: white;
+    cursor: pointer;
+    transition: background-color 0.3s;
+}
+
+.login-form button:hover {
+    background-color: #45a049;
+}
+
+@media (max-width: 600px) {
+    .login-container {
+        margin: 20px;
+        width: 90%;
+    }
 }
 </style>
 
